63dc9203e109a8609f50803db0f990243b2c9bf7
xb <892536707@qq.com>
2020-07-28 11:11:17 +0800

    初始化master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Link to="/home">go Home</Link>
+      <Switch>
+        <Route path="/home">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Home() {
+  return <h1>我是Home</h1>;
+}
+
+export default Home;
