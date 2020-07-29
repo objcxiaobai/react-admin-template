bd1c6d22d348816cd15972e8ef302f08bc523059
xb <892536707@qq.com>
2020-07-29 15:57:23 +0800

    add: add axios

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,25 +1,69 @@
 import React, { Component } from 'react';
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button, message } from 'antd';
 import './index.css';
 class Login extends Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleError = this.handleError.bind(this);
+  }
   render() {
     return (
       <div className="login-container">
-        <Form className="content">
-          <Form.Item label="账号">
+        <Form
+          className="content"
+          onFinish={this.handleSubmit}
+          onFinishFailed={this.handleError}
+        >
+          <Form.Item
+            name="username"
+            label="账号"
+            rules={[
+              {
+                required: true,
+                message: 'please input your username',
+              },
+            ]}
+          >
             <Input />
           </Form.Item>
-          <Form.Item label="密码">
-            <Input />
+          <Form.Item
+            name="password"
+            label="密码"
+            rules={[
+              {
+                required: true,
+                message: 'please input your password',
+              },
+            ]}
+          >
+            <Input.Password />
           </Form.Item>
           <Form.Item>
-            <Button type="submit" className="login-container-btn">
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-container-btn"
+            >
               登录
             </Button>
           </Form.Item>
         </Form>
       </div>
     );
+  }
+
+  handleSubmit(event) {
+    console.log(event);
+    const { username, password } = event;
+    if (username == 'xb' && password == '123') {
+      message.success('登录成功');
+    } else {
+      message.error('账号密码错误');
+    }
+  }
+  handleError(error) {
+    console.log('失败', error);
   }
 }
 
