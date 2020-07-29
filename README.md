f4ac3762011a4c945eadd604e1bfad2e003a207a
xb <892536707@qq.com>
2020-07-29 10:56:19 +0800

    add: header tag in layout

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 - 使用对象 key 形式解决 `antd`v4 版本 动态 icon 渲染问题
 - side 添加 Logo
 - ssh
+
+### 0.0.2
+
+- 在 Layout 中添加 `header`
---
+++ src/pages/layout/Header/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Layout, Dropdown, Menu, Avatar } from 'antd';
+import './index.css';
+import duoduo from '../../../assets/images/duoduo.jpg';
+import { CaretDownOutlined } from '@ant-design/icons';
+
+const { Header } = Layout;
+
+class LayoutHeader extends Component {
+  render() {
+    const menu = (
+      <Menu onClick={this.handleClick}>
+        <Menu.Item key="">
+          <Link to="/dashboard">首页</Link>
+        </Menu.Item>
+        <Menu.Item key="logo">注销</Menu.Item>
+      </Menu>
+    );
+
+    return (
+      <>
+        <Header>
+          <div className="right-menu">
+            <div className="dropdown-wrap">
+              <Dropdown overlay={menu}>
+                <div>
+                  <Avatar shape="square" size="medium" src={duoduo}></Avatar>
+                  <CaretDownOutlined />
+                </div>
+              </Dropdown>
+            </div>
+          </div>
+        </Header>
+      </>
+    );
+  }
+
+  handleClick(key) {
+    console.log(key);
+  }
+}
+
+export default LayoutHeader;
