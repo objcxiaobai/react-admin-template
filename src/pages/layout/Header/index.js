dbb5b81d06759731e1b751a586d5ca1a4f164312
xb <892536707@qq.com>
2020-07-30 16:45:31 +0800

    update:login out

--- src/pages/layout/Header/index.js
+++ src/pages/layout/Header/index.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Layout, Dropdown, Menu, Avatar } from 'antd';
-import './index.css';
 import duoduo from '../../../assets/images/duoduo.jpg';
 import { CaretDownOutlined } from '@ant-design/icons';
 import Hamburger from '../../../components/Hamburger';
+import { connect } from 'react-redux';
+import { actionCreators } from '../../../pages/login/store';
+import './index.css';
 const { Header } = Layout;
 
 class LayoutHeader extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
   render() {
     const menu = (
       <Menu onClick={this.handleClick}>
         <Menu.Item key="">
           <Link to="/dashboard">首页</Link>
         </Menu.Item>
-        <Menu.Item key="logo">注销</Menu.Item>
+        <Menu.Item key="outLogin">注销</Menu.Item>
       </Menu>
     );
 
@@ -37,9 +44,20 @@ class LayoutHeader extends Component {
     );
   }
 
-  handleClick(key) {
-    console.log(key);
+  handleClick(item) {
+    let key = item.key;
+    switch (key) {
+      case 'outLogin':
+        this.props.loginOut();
+        break;
+    }
   }
 }
 
-export default LayoutHeader;
+const mapDispatchToProps = (dispatch) => ({
+  loginOut() {
+    dispatch(actionCreators.loginOut());
+  },
+});
+
+export default connect(null, mapDispatchToProps)(LayoutHeader);
--- src/pages/login/store/constants.js
+++ src/pages/login/store/constants.js
@@ -1,1 +1,2 @@
 export const CHANGE_TOKEN = 'login/CHANGE_TOKEN';
+export const LOGIN_OUT = 'login_out';
