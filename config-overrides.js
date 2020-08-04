392956bd310f9c3939fe5890a1b52cbea0447aa7
xb <892536707@qq.com>
2020-08-04 16:00:15 +0800

    update:package.json

---
+++ config-overrides.js
@@ -0,0 +1,13 @@
+const {
+  override,
+  addWebpackAlias,
+  addDecoratorsLegacy,
+} = require('customize-cra');
+const path = require('path');
+
+module.exports = override(
+  // 配置路径别名
+  addWebpackAlias({
+    ['@']: path.resolve(__dirname, 'src'),
+  })
+);
--- src/config/routeMap.js
+++ src/config/routeMap.js
@@ -1,17 +1,17 @@
 import Loadable from 'react-loadable';
 import { Spin } from 'antd';
 const Dashboard = Loadable({
-  loader: () => import(/*webpackChunkName:'Dashboard'*/ '../pages/dashboard'),
+  loader: () => import(/*webpackChunkName:'Dashboard'*/ '@/pages/dashboard'),
   loading: Spin,
 });
 
 const Documents = Loadable({
-  loader: () => import(/*webpackChunkName:'Documents'*/ '../pages/documents'),
+  loader: () => import(/*webpackChunkName:'Documents'*/ '@/pages/documents'),
   loading: Spin,
 });
 
 const Permission = Loadable({
-  loader: () => import(/*webpackChunkName:'Permission'*/ '../pages/permission'),
+  loader: () => import(/*webpackChunkName:'Permission'*/ '@/pages/permission'),
   loading: Spin,
 });
 
